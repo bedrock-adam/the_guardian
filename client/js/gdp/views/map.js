b88c671dc086b9a558f601e7e274284a666b8d5c
Adam Mikulasev <adam@dealfetch.com.au>
2015-03-22 22:50:12 +1100

    refactor imperative render loop in ga.gdp.MapView into function call

--- client/js/gdp/views/map.js
+++ client/js/gdp/views/map.js
@@ -2,33 +2,34 @@
   "use strict";
 
   var MapView = Backbone.View.extend({
-    render: function() {
-      var map = new GMaps({
+    initialize: function() {
+      this.bounds = [];
+      this.map = new GMaps({
         el: this.el,
-        lat: -25.274398,
-        lng: 133.775136
+        lat: 0,
+        lng: 0
       });
+    },
+    renderOne: function(country) {
+      var latlng = new google.maps.LatLng(country.lat, country.lng);
+      this.bounds.push(latlng);
 
-      var bounds = [];
-
-      this.collection.toJSON().forEach(function(country) {
-        var latlng = new google.maps.LatLng(country.lat, country.lng);
-        bounds.push(latlng);
-
-        map.addMarker({
-          lat: country.lat,
-          lng: country.lng,
-          title: country.countryName,
-          mouseover: function(e) {
-            this.infoWindow.open(this.map, this);
-          },
-          infoWindow: {
-            content: '<p>' + country.countryName + '</p>'
-          }
-        });
+      this.map.addMarker({
+        lat: country.lat,
+        lng: country.lng,
+        title: country.countryName,
+        mouseover: function(e) {
+          this.infoWindow.open(this.map, this);
+        },
+        infoWindow: {
+          content: '<p>' + country.countryName + '</p>'
+        }
       });
+    },
+    render: function() {
+      Array.prototype.forEach.call(this.collection.toJSON(), this.renderOne, this);
 
-      map.fitLatLngBounds(bounds);
+      this.map.fitLatLngBounds(this.bounds);
     }
   });
 
