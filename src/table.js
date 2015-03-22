a8f36e383723f55052d0b796ff9588b030473d5a
Adam Mikulasev <adam@dealfetch.com.au>
2015-03-22 14:46:47 +1100

    remove dependency on underscore (Array.prototype contains map and filter methods)

--- src/table.js
+++ src/table.js
@@ -1,5 +1,3 @@
-var _ = require('underscore');
-
 var countryCode = function(row) {
   return row[0];
 };
@@ -9,7 +7,7 @@ var isCountry = function(row) {
 };
 
 var countries = function(table) {
-  return _.filter(table, isCountry);
+  return Array.prototype.filter.call(table, isCountry);
 };
 
 var gdp = function(row) {
@@ -28,7 +26,7 @@ var formatRow = function(row) {
 }
 
 var formatTable = function(table) {
-  return _.map(countries(table), formatRow);
+  return Array.prototype.map.call(countries(table), formatRow);
 };
 
 module.exports = {
